e8778b8575bb10d31eeca76b570c6bf99337b10a
Dinesh Makkar <dineshmakkar079@gmail.com>
2017-06-13 20:39:45 +0530

    Removed hostcode variable as required.

--- js/myscript.js
+++ js/myscript.js
@@ -1,92 +1,81 @@
 $(document).ready(function() {
     // Finding isbn and book name + author name
     var hostname = location.hostname.toLowerCase();
-    const AMAZON = { hostname : "amazon" , hostcode : "A" };
-    const FLIPKART = { hostname : "flipkart" , hostcode : "F" };
-    const SNAPDEAL = { hostname : "snapdeal" , hostcode : "S" };
-    const NOT_VALID = { hostname : "invalid" , hostcode : "N" }; // For hosts that are not allowed.
-    const INVALID = "INVALID"; 
+    const AMAZON = "amazon";
+    const FLIPKART = "flipkart";
+    const SNAPDEAL = "snapdeal";
+    const INVALID = "invalid"; 
     
-    var hostcode = NOT_VALID.hostcode;
-    if(hostname.indexOf(AMAZON.hostname) !== -1){
-        hostcode = AMAZON.hostcode;
-    }else if(hostname.indexOf(FLIPKART.hostname) !== -1){
-        hostcode = FLIPKART.hostcode;
-    }else if(hostname.indexOf(SNAPDEAL.hostname) !== -1){
-        hostcode = SNAPDEAL.hostcode;
-    }
-    
-    if(hostcode !== NOT_VALID.hostcode){ //Don't parse if website is other than those of specified.
-        var i13 , total , author, name;
-        if(hostcode === AMAZON.hostcode){ // Proceed as follows if amazon
-            i13 = $("li:contains('ISBN-13')").first().text().replace("ISBN-13:","").trim();
-            
-            name = $("#productTitle").first().text().trim();
-            // Alternates when viewing kindle books
-            if(name == "")
-            {
-                name = $("#ebooksProductTitle").first().text().trim();
-            }
-            
-            author =  $(".author > a").first().text().trim();
-            
-            // Alternates when viewing kindle books
-            if (author == "")
-            {
-                author = $(".contributorNameID").first().text();
-            }
-            // Concatinating for final search
-            total = name + " " + author;
-            total = total.trim();
-        }else if(hostcode === FLIPKART.hostcode){
-            let infoList = $("._3WHvuP > ul").find("li");
-            let len = infoList.length;
-
-            name = location.pathname.split("/")[1].split("-").join(" ");
-            author = $(".PWqzqY").get(0).innerText;
-            total = name + " " + author;
-            for(let i = 0 ; i < len ; i++)
-            {
-                let str = infoList[i].innerText; 
-                if(str.indexOf("ISBN") !== -1) 
-                {
-                    i13 = str.split(":")[1].split(",")[0].trim();
-                    break;
-                } 
-            }
-        }else if(hostcode === SNAPDEAL.hostcode){
-            let infoList = $(".h-content");
-            let len = infoList.length;
-            name = $(".pdp-e-i-head").get(0).innerText;
-            author = "";
-            let authorFound = false, isbnFound = false;
-            for(let i = 0 ; i < len ; i++)
-            { 
-                let str = infoList[i].innerText; 
-                if(str.indexOf("Author") !== -1){
-                    author = str.split(":")[1].trim();
-                    authorFound = true;
-                }
-                if(str.indexOf("ISBN13") !== -1) 
-                { 
-                    i13 = str.split(":")[1].trim();
-                    isbnFound = true; 
-                }
-                if(authorFound && isbnFound) 
-                {
-                    break;
-                }
-            }
-            total = name + " " + author;
+    var i13 , total , author, name;
+    if(hostname.indexOf(AMAZON) !== -1){ // Proceed as follows if amazon
+        i13 = $("li:contains('ISBN-13')").first().text().replace("ISBN-13:","").trim();
+        
+        name = $("#productTitle").first().text().trim();
+        // Alternates when viewing kindle books
+        if(name == "")
+        {
+            name = $("#ebooksProductTitle").first().text().trim();
         }
-        // Listener to send the value to the extension when extension popup clicked
-        chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
-            if(hostcode !== NOT_VALID.hostcode){
-                sendResponse({i13:i13,total:total});
-            }else{
-                sendResponse({i13:INVALID , total: INVALID});
+        
+        author =  $(".author > a").first().text().trim();
+        
+        // Alternates when viewing kindle books
+        if (author == "")
+        {
+            author = $(".contributorNameID").first().text();
+        }
+        // Concatinating for final search
+        total = name + " " + author;
+        total = total.trim();
+    }else if(hostname.indexOf(FLIPKART) !== -1){
+        let infoList = $("._3WHvuP > ul").find("li");
+        let len = infoList.length;
+
+        name = location.pathname.split("/")[1].split("-").join(" ");
+        author = $(".PWqzqY").get(0).innerText;
+        total = name + " " + author;
+        for(let i = 0 ; i < len ; i++)
+        {
+            let str = infoList[i].innerText; 
+            if(str.indexOf("ISBN") !== -1) 
+            {
+                i13 = str.split(":")[1].split(",")[0].trim();
+                break;
+            } 
+        }
+    }else if(hostname.indexOf(SNAPDEAL) !== -1){
+        let infoList = $(".h-content");
+        let len = infoList.length;
+        name = $(".pdp-e-i-head").get(0).innerText;
+        author = "";
+        let authorFound = false, isbnFound = false;
+        for(let i = 0 ; i < len ; i++)
+        { 
+            let str = infoList[i].innerText; 
+            if(str.indexOf("Author") !== -1){
+                author = str.split(":")[1].trim();
+                authorFound = true;
             }
-        });
+            if(str.indexOf("ISBN13") !== -1) 
+            { 
+                i13 = str.split(":")[1].trim();
+                isbnFound = true; 
+            }
+            if(authorFound && isbnFound) 
+            {
+                break;
+            }
+        }
+        total = name + " " + author;
     }
+
+    // Listener to send the value to the extension when extension popup clicked
+    chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if(hostname.indexOf(AMAZON) !== -1 || hostname.indexOf(FLIPKART) !== -1 || hostname.indexOf(SNAPDEAL) !== -1){
+            sendResponse({i13:i13,total:total});
+        }else{
+            sendResponse({i13:INVALID , total: INVALID});
+        }
+    });
 });
